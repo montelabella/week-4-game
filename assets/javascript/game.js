c90f9b864e4c2266858d20c12cfeb5184f05a35a
Crystal Hughes <hugachia@me.com>
2018-02-10 15:56:57 -0600

    add image

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+//need 4 crystals with random result//
+var randomResult;
+//4 crystal needs a random result between 1-12//
+for (i = 0; i < 4; i++) {
+    var crystal = $("")
+}
+
+//need target nuimber//
+var targetNumber;
+//need current score//
+var CurrentScore;
+//need wins/losses score//
+var loss;
+var win;
+//need random value of each crystal//
+//when target is reached declare win//
+//when target is over declare loss//
+//clicking on crystal accumulates points//
