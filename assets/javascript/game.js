b775308b07f4f0e716b730548be8bae6f5ec33bf
Crystal Hughes <hugachia@me.com>
2018-02-10 20:14:55 -0600

    stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,33 @@
 
 //need 4 crystals with random result//
 var randomResult;
+var totalScore;
+
+//getter//
+$(".crystalImage").attr('class');
 //4 crystal needs a random result between 1-12//
-for (i = 0; i < 4; i++) {
-    var crystal = $("")
+randomResult = Math.floor(Math.random() * 101) + 19;
+console.log(randomResult);
+
+$("#yts").html('Your Goal: '+ randomResult);
+for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 11) + 1;
+    console.log(random);
+
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystalImage',
+        "data-random": random
+    });
+    $(".crystalImage").append(crystal);
+
 }
+$("crystal").on('click', function (){
+     var result;
+     var num = $(this).attr('data-random');
+
+    console.log($(this).attr('data-random'));
+});
 
 //need target nuimber//
 var targetNumber;
