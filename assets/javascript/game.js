6ea8f6c318676f37924b1329aff47d1cc582c3c0
Crystal Hughes <hugachia@me.com>
2018-02-10 23:37:03 -0600

    final again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -59,47 +59,9 @@ function updateGameCount(win){
 	}else{
 		winCount++;
 	}
-	$('#scoreboard').text("Wins: " + winCount + "<br>Losses: " + lossCount);
+	$('#scoreboard').text("Wins: " + winCount + " Losses: " + lossCount);
 	startGame();
 	
 }
 
-/*CRAP I GAVE UP ON ////
-//getter//
-$(".crystalImage").attr('class');
-//4 crystal needs a random result between 1-12//
-randomResult = Math.floor(Math.random() * 101) + 19;
-console.log(randomResult);
 
-$("#yts").html('Your Goal: '+ randomResult);
-for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 11) + 1;
-    console.log(random);
-
-  var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystalImage',
-        "data-random": random
-    });
-//    $(".crystalImage").append(crystal);
-
-}
-$(".crystalImage").click(function (){
-     var result;
-     num = $(this).attr('data-random');
-     alert(num);
-    console.log($(this).attr('data-random'));
-});
-
-//need target nuimber//
-var targetNumber;
-//need current score//
-var CurrentScore;
-//need wins/losses score//
-var loss;
-var win;
-//need random value of each crystal//
-//when target is reached declare win//
-//when target is over declare loss//
-//clicking on crystal accumulates points//
-*/
